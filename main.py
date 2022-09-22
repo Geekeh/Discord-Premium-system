073434b3f0d6c0162e3c2ff3e022222de8ebad2d
Geek <98252854+Geekeh@users.noreply.github.com>
2022-09-22 01:59:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,51 +5,65 @@ from datetime import datetime
 from datetime import timedelta
 import datetime
 import discord
-import uuid
 from pymongo import MongoClient
 
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-mongo_db_link = 'replace this with our mongodb access link'
+#all your mongo db information
+#mongo_db_link = 'Put your mongo db connection string'
+#databases_name = 'Put your database name here' # -- Example https://ibb.co/1fpQktR
+#collection_name = "Put your database collection name here" # -- Example https://ibb.co/Fwwvbxw
+
+#key information
+role_name = "Buyer"
+key_prefix = "Discord" # -- Example: Discord-byXjAI
 
 @bot.command()
+@commands.has_permissions(manage_roles=True)  
 async def gen(ctx, amount, time):
    key_int = int(amount)
-   amount = key_int - 1
+   amount = key_int
    key_amt = range(int(amount))
    time = int(time)
 
    # -- Connect to database n shit
    mongo_url = mongo_db_link
    cluster = MongoClient(mongo_url)
-   db = cluster["discord"]
-   collection = db["discord"]
+   db = cluster[f"{databases_name}"]
+   collection = db[f"{collection_name}"]
 
    # -- Expiration
    now = datetime.datetime.today()
    future = now + timedelta(days=time)
-   expires = future.strftime("%m-%d")
+   expires = future.strftime("%y-%m-%d")
 
    # -- Key
-   key_yes = 'discord-fkEPsG'
+   key_yes = f"{key_prefix}-fkEPsG"
    if key_int == 1:
       letters = string.ascii_letters
-      key = "discord-" + ''.join(random.choice(letters) for i in range(6))
+      key = f"{key_prefix}-" + ''.join(random.choice(letters) for i in range(6))
    elif key_int < 1:
       em = discord.Embed(color=0xff0000)
       em.add_field(name="Invalid number", value="Key amount needs to be higher than 0")
       await ctx.send(embed=em)
       return 0
    elif key_int > 1:
+      amount = key_int - 1
+      key_number = 1
+      key_amt = range(int(amount))
       for i in key_amt:
          letters = string.ascii_letters
-         key = "discord-" + ''.join(random.choice(letters) for i in range(6))
+         key = f"{key_prefix}-" + ''.join(random.choice(letters) for i in range(6))
          em = discord.Embed(color=0xff0000)
-         em.add_field(name="Key", value=key)
+         em.add_field(name=f"Key: {key_number}", value=key)
          await ctx.send(embed=em)
+         key_number += 1
+         post = {"key": key, "expiration": expires, "user": "Empty", "used": 'unused'}
+         collection.insert_one(post)
+      key = f"{key_prefix}-" + ''.join(random.choice(letters) for i in range(6))
    
-   # -- collection.delete_many({})
+   # -- collection.delete_many({}) #deletes all the keys
 
    # -- Send all info to discord and database
    message = await ctx.send("Connecting...")
@@ -57,10 +71,15 @@ async def gen(ctx, amount, time):
       if key_int == 1:
          key_yes = key
       else:
-         key_yes = 'discord-fkEPsG'
+         key_yes = f'{key_prefix}-fkEPsG'
          pass
+
+      #sends key info to database
+      
       post = {"key": key, "expiration": expires, "user": "Empty", "used": 'unused'}
       collection.insert_one(post)
+
+      #make our embed that sends when key is genned
       em = discord.Embed(color=0x00ff00)
       em.add_field(name="\n" + "discord", value="**Key generated!**" + "\n" +
          "key: " + key + "\n" + "Expires: " + str(time) + " days" +  "\n" + "\n"
@@ -72,12 +91,13 @@ async def gen(ctx, amount, time):
       em.add_field(name="Api did not respond", value="Could not generate key")
       await ctx.send(content="", embed=em)
 
+#redeem function
 @bot.command()
 async def redeem(ctx, key):
    mongo_url = mongo_db_link
    cluster = MongoClient(mongo_url)
-   db = cluster["discord"]
-   collection = db["discord"]
+   db = cluster[f"{databases_name}"]
+   collection = db[f"{collection_name}"]
    try:
       results = collection.find({"key": key})
       for results in results:
@@ -90,7 +110,7 @@ async def redeem(ctx, key):
                em.add_field(name="Key already used", value="the key you entered has already been used")
                await ctx.send(embed=em)
             elif used == 'unused':
-               role = 'Buyer'
+               role = role_name
                user = ctx.message.author
                await user.add_roles(discord.utils.get(user.guild.roles, name=role))
                em = discord.Embed(title="discord", color=0x00ff00)
@@ -107,6 +127,4 @@ async def redeem(ctx, key):
    except:
       pass
 
-
-
-bot.run("Put your dsicord bot token here")
+bot.run('Discord bot token here')
