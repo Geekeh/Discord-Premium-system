1b003f589ba8b4750326a5c140b88734019b0b36
Geek <98252854+Geekeh@users.noreply.github.com>
2022-09-22 02:10:35 -0500

    Update role.py

--- role.py
+++ role.py
@@ -8,9 +8,12 @@ from pymongo import MongoClient
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 #all your mongo db information
-#mongo_db_link = 'Put your mongo db connection string'
-#databases_name = 'Put your database name here' # -- Example https://ibb.co/1fpQktR
-#collection_name = "Put your database collection name here" # -- Example https://ibb.co/Fwwvbxw
+mongo_db_link = 'Put your mongo db connection string'
+databases_name = 'Put your database name here' # -- Example https://ibb.co/1fpQktR
+collection_name = "Put your database collection name here" # -- Example https://ibb.co/Fwwvbxw
+
+#this is the channel where it will say key expired
+channel_to_send_messages = (Replace this with your channel id)
 
 @bot.command()
 async def enable(ctx):
@@ -32,12 +35,13 @@ async def enable(ctx):
                 current_date = datetime.datetime.today()
                 check_time = datetime.datetime.strptime(expiration, "%y-%m-%d")
                 if current_date > check_time:
+                        channel = bot.get_channel(channel_to_send_messages)
                         em = discord.Embed(color=0x852525)
-                        em.add_field(name='Key expired', value=f'<@{user}> Your key is expired' + "\n" + "Key:" + key)
-                        await ctx.send(embed=em)
+                        em.add_field(name='Key expired', value=f'<@{user}> Your key is expired' + "\n" + "Key: " + key)
+                        await channel.send(embed=em)
                         collection.delete_many({"key": key})
                 else:
                         pass
 
 
-bot.run("discord bot token here")
+bot.run("replace with your discord bot token)
